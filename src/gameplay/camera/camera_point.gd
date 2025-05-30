04c26ffab6321a28d520d0057e79b2bae2cffcfb
scrawach <scrawach@gmail.com>
2025-05-30 14:50:37 +0300

    feat: following camera

--- src/gameplay/camera/camera_point.gd
+++ src/gameplay/camera/camera_point.gd
@@ -1,4 +1,12 @@
 class_name CameraPoint
 extends Node3D
 
+@export var follow_target: Node3D
+
 @onready var camera: Camera3D = %Camera3D
+
+func _physics_process(_delta: float) -> void:
+	if follow_target == null:
+		return
+	
+	global_position = follow_target.global_position
